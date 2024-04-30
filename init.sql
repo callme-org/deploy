ef7d64dd2b443de31a872295d774bc01f37346f1
comougi <comougi@gmail.com>
2024-04-30 17:45:42 +0300

    update init.sql

--- init.sql
+++ init.sql
@@ -4,47 +4,45 @@ CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
 FLUSH PRIVILEGES;
 
-USE ${MYSQL_DATABASE};
-
-CREATE TABLE turnusers_lt (
+CREATE TABLE ${MYSQL_DATABASE}.turnusers_lt (
 realm varchar(127) default '',
 name varchar(512),
 hmackey char(128),
 PRIMARY KEY (realm,name)
 );
 
-CREATE TABLE turn_secret (
+CREATE TABLE ${MYSQL_DATABASE}.turn_secret (
     realm varchar(127) default '',
     value varchar(256),
     primary key (realm,value)
 );
 
-CREATE TABLE allowed_peer_ip (
+CREATE TABLE ${MYSQL_DATABASE}.allowed_peer_ip (
     realm varchar(127) default '',
     ip_range varchar(256),
     primary key (realm,ip_range)
 );
 
-CREATE TABLE denied_peer_ip (
+CREATE TABLE ${MYSQL_DATABASE}.denied_peer_ip (
     realm varchar(127) default '',
     ip_range varchar(256),
     primary key (realm,ip_range)
 );
 
-CREATE TABLE turn_origin_to_realm (
+CREATE TABLE ${MYSQL_DATABASE}.turn_origin_to_realm (
     origin varchar(127),
     realm varchar(127),
     primary key (origin)
 );
 
-CREATE TABLE turn_realm_option (
+CREATE TABLE ${MYSQL_DATABASE}.turn_realm_option (
     realm varchar(127) default '',
     opt varchar(32),
     value varchar(128),
     primary key (realm,opt)
 );
 
-CREATE TABLE oauth_key (
+CREATE TABLE ${MYSQL_DATABASE}.oauth_key (
     kid varchar(128),
     ikm_key varchar(256),
     timestamp bigint default 0,
@@ -54,7 +52,7 @@ CREATE TABLE oauth_key (
     primary key (kid)
 );
 
-CREATE TABLE admin_user (
+CREATE TABLE ${MYSQL_DATABASE}.admin_user (
     name varchar(32),
     realm varchar(127),
     password varchar(127),
